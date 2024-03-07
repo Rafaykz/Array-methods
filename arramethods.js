04e9121237cbf3e953e0207cdcdfd303e452d15c
Rafaykz <kzr7091@gmail.com>
2024-03-07 15:02:53 +0500

    Array methods

---
+++ arramethods.js
@@ -0,0 +1,86 @@
+// array to string methods 
+let num = [1,2,3,4,];
+let b = num.toString();
+document.write( b );
+// array to string methods 
+document.write("<br><br>")
+
+// array to join methods 
+let c = num.join("_");
+document.write( c );
+// array to join methods 
+document.write("<br><br>")
+// array to pop methods 
+// let d = num.pop()
+// document.write(num)
+// // array to pop methods 
+ 
+document.write("<br><br>")
+// array to push methods 
+let e = num.push(56)
+document.write(num)
+// array to pop methods 
+
+document.write("<br><br>")
+// array to shift methods 
+let f = num.shift()
+console.log(f, num)
+// array to shift methods 
+
+document.write("<br><br>")
+// array to anshift methods 
+let g = num.unshift(78)
+console.log(g , num)
+// array to anshift methods 
+
+
+
+document.write("<br><br>")
+// array to anshift methods 
+let num2 = [1,2,3,4,5,6,7,8,9]
+delete num2[0]
+document.write( num2)
+// array to anshift methods 
+
+
+
+document.write("<br><br>")
+// array to anshift methods 
+let new1 = [1,2,3,4,5,6,7,8,9]
+let new2= [12,23,34,45,56,67,78,89,90]
+let new3 = [111,222,333,444,555,666,777,888,999]
+let new4 = new1.concat(new2,new3)
+document.write(new4)
+// array to anshift methods 
+
+
+
+document.write("<br><br>")
+// array to anshift methods 
+let num3 = [3,12,23,64,85,16,27,8,9]
+num3.sort()
+// num3.reverse()
+document.write( num3)
+// array to anshift methods 
+
+
+document.write("<br><br>")
+// array to anshift methods 
+let num4 = [300,12,23,64,85,16,27,8,9]
+num4.splice(2, 3 , 567 , 876 , 786 )
+document.write( num4)
+// array to anshift methods
+
+
+
+
+
+
+
+
+
+
+
+
+
+
